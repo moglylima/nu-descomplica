9bf448ceb1b19ba33d19acba03d9b1b24c986e69
Mogleson Lima <moglesonlima@gmail.com>
2022-10-22 00:52:10 -0300

    chore(projetocarrinho/): Finalizando projeto.

--- javascript/projetocarrinho/js/cart.js
+++ javascript/projetocarrinho/js/cart.js
@@ -1,3 +1,40 @@
 var valorTotal = [0, 0];
 var valorProduto = [50.0, 30.0];
 var quantidadeProduto = [0, 0];
+
+function adicionarProduto(posicao) {
+  var quantidade = document.getElementById("quantidade" + posicao);
+
+  var total = document.getElementById("total" + posicao);
+  quantidadeProduto[posicao] += 1;
+  valorTotal[posicao] =
+    Number.parseFloat(valorProduto[posicao]) * quantidadeProduto[posicao];
+  quantidade.innerHTML = quantidadeProduto[posicao];
+  total.innerHTML = valorTotal[posicao].toFixed(2);
+
+  valorTotalCompra();
+}
+
+function removerProduto(posicao) {
+  var quantidade = document.getElementById("quantidade" + posicao);
+  var total = document.getElementById("total" + posicao);
+  if (quantidadeProduto[posicao] > 0) {
+    quantidadeProduto[posicao] -= 1;
+    valorTotal[posicao] =
+      Number.parseFloat(valorProduto[posicao]) * quantidadeProduto[posicao];
+    quantidade.innerHTML = quantidadeProduto[posicao];
+    total.innerHTML = valorTotal[posicao].toFixed(2);
+  }
+  valorTotalCompra();
+}
+
+function valorTotalCompra() {
+  var total = document.getElementById("subtotalcompra");
+  var valor = 0;
+
+  for (var i = 0; i < valorTotal.length; i++) {
+    valor += valorTotal[i];
+  }
+
+  total.innerHTML = valor.toFixed(2);
+}
